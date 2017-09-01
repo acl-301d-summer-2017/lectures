e358a8c190d958f69000bebd3ad68372d1d0f0ab
Sarah Joy Calpo <sajoy.pc@gmail.com>
2017-09-01 12:05:01 -0700

    update d5

--- lectures/05-ajax-and-json-and-wrrc/demos/ajax-local-file/app.js
+++ lectures/05-ajax-and-json-and-wrrc/demos/ajax-local-file/app.js
@@ -1,16 +1,39 @@
 'use strict';
 
-
+// TODO make AJAX request on click
 // TODO use AJAX to load data from a JSON file
-// $.ajax()
-// $.get()
-// $.getJSON()
+// $.ajax({
+//     url: 'data.json',
+//     method: 'GET',
+//     success: function ( data ) {
+//         console.log( 'successfully got the json file' );
+//     }
+// });
 
-// TODO cache our JSON file in localStorage 
+
+// $.get( 'data.json', function ( hufflepuff ) {
+//     console.log( 'got the stuff', hufflepuff );
+// });
+
+if ( localStorage.data ) {
+    appendToDOM( JSON.parse( localStorage.data ) );
+} else {
+    $.getJSON( 'data.json' )
+        .done( function ( data ) {
+            console.log( 'got the stuff?', data );
+            localStorage.setItem( 'data', JSON.stringify( data ) );
+            appendToDOM( data );
+        })
+        .fail( function ( ravenclaw ) {
+            console.log( 'something bad', ravenclaw );
+        })
+        .always( function ( slytherin ) {
+            console.log( 'all the time!!', slytherin );
+        });
+}
+    
+// TODO cache our JSON file in localStorage
 // TODO only make the AJAX request if it's not in localStorage
-
-// TODO use let and var
-// TODO use template literals
 
 
 function appendToDOM ( data ) {
@@ -23,3 +46,37 @@ function appendToDOM ( data ) {
         });
     });
 }
+
+
+
+// ES2015 new things!
+// TODO use let and const
+
+var day = 'Friday!';
+console.log( day );
+day = 'Monday ):';
+console.log( day );
+
+const month = 'September';
+console.log( month );
+// month = 'October';
+// console.log( month );
+
+const cat = { name: 'Ferby', color: 'gray' };
+console.log( cat );
+cat.name = 'James';
+console.log( cat );
+
+// cat = 'cat';
+
+let hour = 'Lunch';
+console.log( hour );
+hour = 'still lecture, sorry';
+console.log( hour );
+
+
+
+// TODO use template literals
+var plant = { name: 'george', color: 'light green' };
+console.log( 'This plant\'s name is ' + plant.name + '. it\'s color is ' +  plant.color );
+console.log( `This plant's name is ${plant.name}. it's color is ${plant.color}` );
