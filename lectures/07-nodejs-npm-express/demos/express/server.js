9dbfc8b4a9dc1ccf055050a92241e2d4c9c8d41b
Sarah Joy Calpo <sajoy.pc@gmail.com>
2017-09-05 12:11:16 -0700

    update class 7 demo

--- lectures/07-nodejs-npm-express/demos/express/server.js
+++ lectures/07-nodejs-npm-express/demos/express/server.js
@@ -1,5 +1,44 @@
-'use strict'
+'use strict';
+
+
+console.log( 'I am in the terminal!!!' );
 
 // TODO require and initialize express
+/* <script src="express.js"></script> */
+const express = require( 'express' );
+const app = express();
+const PORT = process.env.PORT || '4577';
+
+// Tell app what folder has our files to serve
+app.use( express.static( './public' ) );
+
+/*
+$('element').on( 'click', function () {}); 
+'click' is the event to listen for
+*/
 
 // TODO create routes for /cat page
+app.get( '/cat', function ( request, response ) {
+    console.log( 'request', request );
+
+    console.log( 'response', response );
+
+
+    const cats = 'CAT';
+    console.log( 'I am in the terminal!!!' );
+    console.log( cats );
+    response.sendFile( 'cats4life.html', { root: './public' });
+});
+
+// app.get( '/login', function ( request, response ) {
+//     response.sendFile( 'login.html', { root: './public' });
+// });
+
+// app.get( '/profile', function ( request, response ) {
+//     if ( user.isSarah ) { // do something }
+//     response.sendFile( 'profile.html', { root: './public' });
+// });
+
+app.listen( PORT, function () {
+    console.log( `You are one PORT: ${PORT}` );
+});
